4fa9e7c9a81eb0fc6f6300abc4de3ff3277b964a
Stratos Karafotis <stratosk@semaphore.gr>
2014-07-03 06:28:22 -0400

    cpufreq: Introduce new relation for freq selection Introduce CPUFREQ_RELATION_C for frequency selection. It selects the frequency with the minimum euclidean distance to target. In case of equal distance between 2 frequencies, it will select the greater freq.
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>
    
    ----------
    Proper cherry-pick, for if someone else would like to use the commit.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -180,11 +180,17 @@ static inline unsigned long cpufreq_scale(unsigned long old, u_int div, u_int mu
  *                          CPUFREQ GOVERNORS                        *
  *********************************************************************/
 
+<<<<<<< HEAD
 #define CPUFREQ_GOV_START	1
 #define CPUFREQ_GOV_STOP	2
 #define CPUFREQ_GOV_LIMITS	3
 #define CPUFREQ_GOV_POLICY_INIT	4
 #define CPUFREQ_GOV_POLICY_EXIT	5
+=======
+#define CPUFREQ_RELATION_L 0  /* lowest frequency at or above target */
+#define CPUFREQ_RELATION_H 1  /* highest frequency below or at target */
+#define CPUFREQ_RELATION_C 2  /* closest frequency to target */
+>>>>>>> 5c605e8... cpufreq: Introduce new relation for freq selection Introduce CPUFREQ_RELATION_C for frequency selection. It selects the frequency with the minimum euclidean distance to target. In case of equal distance between 2 frequencies, it will select the greater freq.
 
 struct cpufreq_governor {
 	char	name[CPUFREQ_NAME_LEN];
